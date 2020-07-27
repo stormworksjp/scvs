3def4e32f8b4d2cb2ba92bc90ffe3ea9c0c617b3
Neiro Somemiya <smmy@neiro.work>
2020-07-27 20:52:11 +0900

    react導入

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import dotenv from 'dotenv';
+dotenv.config();
+
+class SCVS {
+  public start() {
+    console.log('hello world');
+  }
+}
+
+const scvs = new SCVS();
+scvs.start();
